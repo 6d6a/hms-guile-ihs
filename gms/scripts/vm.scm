1d32dc95cbdd7b3173b6cdb805f1648310882ad1
Oleg Pykhalov <go.wigust@gmail.com>
2018-09-21 10:57:00 +0300

    gms: scripts: Add vm.
    * gms/scripts/vm.scm: New file.

---
+++ gms/scripts/vm.scm
@@ -0,0 +1,142 @@
+;;; Guile GMS --- GMS command-line interface.
+;;; Copyright © 2018 Oleg Pykhalov <go.wigust@gmail.com>
+;;;
+;;; This file is part of Guile GMS.
+;;;
+;;; Guile GMS is free software; you can redistribute it and/or modify
+;;; it under the terms of the GNU General Public License as published
+;;; by the Free Software Foundation; either version 3 of the License,
+;;; or (at your option) any later version.
+;;;
+;;; Guile GMS is distributed in the hope that it will be useful, but
+;;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;;; General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License
+;;; along with Guile GMS.  If not, see <http://www.gnu.org/licenses/>.
+
+(define-module (gms scripts vm)
+  #:use-module ((guix scripts) #:select (parse-command-line))
+  #:use-module ((guix ui) #:select (G_ leave))
+  #:use-module (guix import utils)
+  #:use-module (gms scripts)
+  #:use-module (gms ui)
+  #:use-module (json)
+  #:use-module (rnrs bytevectors)
+  #:use-module (ice-9 match)
+  #:use-module (srfi srfi-1)
+  #:use-module (srfi srfi-11)
+  #:use-module (srfi srfi-26)
+  #:use-module (srfi srfi-37)
+  #:use-module (web client)
+  #:use-module (ice-9 popen)
+  #:use-module (ice-9 rdelim)
+  #:export (vm->scm
+            fetch-vm
+            gms-vm))
+
+(define (show-help)
+  (display (G_ "Usage: gms vm [OPTION ...] ACTION [ARG ...]
+Fetch data about user.\n"))
+  (newline)
+  (display (G_ "The valid values for ACTION are:\n"))
+  (newline)
+  (display (G_ "\
+   show                  show user\n"))
+  (display (G_ "\
+   ip                    show ip address\n"))
+  (display (G_ "\
+   plan                  show account plan\n"))
+  (display (G_ "\
+   server                show server\n"))
+  (display (G_ "\
+   template              show operating system template\n"))
+  (newline)
+  (display (G_ "
+  -h, --help             display this help and exit"))
+  ;; TODO: version
+  #;(display (G_ "
+  -V, --version          display version information and exit"))
+  (newline)
+  ;; TODO: show-bug-report-information
+  #;(show-bug-report-information))
+
+(define %options
+  ;; Specifications of the command-line options.
+  (list (option '(#\h "help") #f #f
+                 (lambda args
+                   (show-help)
+                   (exit 0)))
+        (option '(#\n "Don't convert addresses (i.e., host addresses, port
+numbers, etc.) to names.") #f #f
+                (lambda (opt name arg result)
+                  (alist-cons 'do-not-resolve? #t result)))))
+
+(define %default-options '())
+
+(define (fetch-vm account)
+  (let* ((port   (apply open-pipe* OPEN_READ "jord-cvm" (list account)))
+         (output (read-string port)))
+    (close-port port)
+    (string-trim-right output #\newline)))
+
+(define (vm->scm account)
+  (assoc-ref (hash-table->alist (json-string->scm (fetch-vm account)))
+             "vds_account"))
+
+(define (parse-sub-command arg result)
+  ;; Parse sub-command ARG and augment RESULT accordingly.
+  (if (assoc-ref result 'action)
+      (alist-cons 'argument arg result)
+      (let ((action (string->symbol arg)))
+        (case action
+          ((ip plan server show template)
+           (alist-cons 'action action result))
+          (else (leave (G_ "~a: unknown action~%") action))))))
+
+(define (process-command command args opts)
+  (for-each (lambda (arg)
+              (let ((vds-account (vm->scm arg)))
+                (case command
+                  ((ip)
+                   (let ((primary-ip-address (assoc-ref vds-account "primary_ip_address")))
+                     (format #t "ip_address: ~a~%" (assoc-ref primary-ip-address "address"))
+                     (format #t "isp_license: ~a~%" (assoc-ref primary-ip-address "isp_license"))
+                     (newline)))
+                  ((plan)
+                   (let ((plan (assoc-ref vds-account "plan")))
+                     (format #t "created: ~a~%" (assoc-ref plan "created"))
+                     (format #t "name: ~a~%" (assoc-ref plan "name"))
+                     (format #t "admin: ~a~%" (assoc-ref plan "adm"))
+                     (newline)))
+                  ((server)
+                   (let* ((host (assoc-ref vds-account "host"))
+                          (server (assoc-ref host "server")))
+                     (format #t "name: ~a~%" (assoc-ref server "name"))
+                     (newline)))
+                  ((show)
+                   (format #t "created: ~a~%" (assoc-ref vds-account "created"))
+                   (format #t "changed: ~a~%" (assoc-ref vds-account "changed"))
+                   (format #t "status: ~a~%" (assoc-ref vds-account "status"))
+                   (format #t "state: ~a~%" (assoc-ref vds-account "state"))
+                   (format #t "template_id: ~a~%" (assoc-ref vds-account "vds_template_id"))
+                   (format #t "client_id: ~a~%" (assoc-ref vds-account "client_id"))
+                   (format #t "vnc_port: ~a~%" (assoc-ref vds-account "vnc_port"))
+                   (newline))
+                  ((template)
+                   (let ((template (assoc-ref vds-account "template")))
+                     (format #t "name: ~a~%" (assoc-ref template "name"))
+                     (format #t "URI: ~a~%" (assoc-ref template "path"))
+                     (newline))))))
+            args))
+
+(define (gms-vm . args)
+  ;; TODO: with-error-handling
+  (let* ((opts (parse-command-line args %options
+                                   (list %default-options)
+                                   #:argument-handler
+                                   parse-sub-command))
+         (args (option-arguments opts))
+         (command (assoc-ref opts 'action)))
+    (process-command command args opts)))
---
+++ tests/vm.scm
@@ -0,0 +1,199 @@
+;;; Guile GMS --- GMS command-line interface.
+;;; Copyright © 2018 Oleg Pykhalov <go.wigust@gmail.com>
+;;;
+;;; This file is part of Guile GMS.
+;;;
+;;; Guile GMS is free software; you can redistribute it and/or modify
+;;; it under the terms of the GNU General Public License as published
+;;; by the Free Software Foundation; either version 3 of the License,
+;;; or (at your option) any later version.
+;;;
+;;; Guile GMS is distributed in the hope that it will be useful, but
+;;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;;; General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License
+;;; along with Guile GMS.  If not, see <http://www.gnu.org/licenses/>.
+
+(define-module (vm)
+  #:use-module (guix tests)
+  #:use-module (gms scripts)
+  #:use-module (gms scripts vm)
+  #:use-module (ice-9 match)
+  #:use-module (srfi srfi-64))
+
+(define test-vm-json
+  "{
+  \"success\": true,
+  \"vds_account\": {
+    \"restricted\": false,
+    \"primary_ip_address\": {
+      \"last_sych_date\": \"2014-06-16 16:43:54\",
+      \"address\": \"78.108.95.38\",
+      \"isp_license\": true,
+      \"synchronize\": false,
+      \"equip_ip_address_id\": \"2588\",
+      \"equip_ip_network_id\": \"6\"
+    },
+    \"client_id\": \"4653\",
+    \"vnc_port\": \"5955\",
+    \"services\": [],
+    \"created\": \"2016-08-17 05:47:24\",
+    \"php_version\": \"php53\",
+    \"vds_host_id\": \"25\",
+    \"template\": {
+      \"adm\": true,
+      \"vds_template_id\": \"22\",
+      \"vds_platform_id\": \"4\",
+      \"isp_license\": true,
+      \"only_for_old_plans\": false,
+      \"path\": \"http://mirror.yandex.ru/debian/dists/jessie/main/installer-amd64/\",
+      \"active\": true,
+      \"name\": \"Debian Linux 8 KVM (ISP Manager)\",
+      \"description\": \"Debian Linux 8 jessie\"
+    },
+    \"host\": {
+      \"vnet\": false,
+      \"active\": true,
+      \"cpu_usage\": \"0\",
+      \"vds_plan_id\": \"41\",
+      \"vds_platform_id\": \"4\",
+      \"equip_server_id\": \"21000\",
+      \"server\": {
+        \"ram_count\": \"18\",
+        \"hdd_type\": \"\",
+        \"server_install_date\": \"\",
+        \"equip_supplier_id\": \"2\",
+        \"server_remove_reason\": \"\",
+        \"cpu_socket_type\": \"LGA1366\",
+        \"server_remove_date\": \"\",
+        \"working_days_count\": \"1678\",
+        \"ram_type\": \"DDR3\",
+        \"manufacturer\": \"Supermicro\",
+        \"created\": \"0000-00-00 00:00:00\",
+        \"ecc_ram\": true,
+        \"registered_ram\": \"\",
+        \"dcid\": \"\",
+        \"hdd_racks\": \"\",
+        \"category\": \"unknown\",
+        \"cpu_count\": \"2\",
+        \"unit_count\": \"2\",
+        \"equip_server_type_id\": \"2\",
+        \"recommended_remove_date\": \"\",
+        \"ram_speed\": \"800 - 1333 ÐÐÑ\",
+        \"cpu_type\": \"Intel Xeon CPU E5620 2.40GHz\",
+        \"datacenter_number\": \"2593\",
+        \"serial_number\": \"\",
+        \"psu_power\": \"\",
+        \"equip_server_id\": \"21000\",
+        \"name\": \"kvm27\",
+        \"cpu_socket_count\": \"2\",
+        \"state\": \"unknown\",
+        \"hdd_count\": \"\",
+        \"colo_platform_id\": \"0\",
+        \"server_warranty\": \"\",
+        \"cost\": \"0.0000\",
+        \"model\": \"X8DAH\",
+        \"equip_rack_id\": \"7\",
+        \"psu_count\": \"\"
+      },
+      \"vds_host_id\": \"25\",
+      \"mac_address\": \"00:25:90:0B:67:C0\",
+      \"cpu_capacity\": \"38400\",
+      \"disk_usage\": \"1246950\",
+      \"mem_capacity\": \"98304\",
+      \"mem_usage\": \"40961\",
+      \"disk_capacity\": \"1709036\"
+    },
+    \"no_auto_enable\": false,
+    \"plan\": {
+      \"adm\": true,
+      \"active\": true,
+      \"vds_plan_id\": \"41\",
+      \"vds_platform_id\": \"4\",
+      \"cpu_capacity\": \"2000\",
+      \"name\": \"A-2\",
+      \"can_add_options\": false,
+      \"created\": \"2014-07-01 16:54:18\",
+      \"ip_address_count\": \"1\",
+      \"disk_capacity\": \"40960\",
+      \"cpu_count\": \"2\",
+      \"mem_capacity\": \"2048\",
+      \"cost\": \"2290.0000\",
+      \"cost_discount\": \"1990.0000\"
+    },
+    \"client_ip\": \"\",
+    \"vds_account_id\": \"17803\",
+    \"equip_ip_address_id\": \"2588\",
+    \"vds_plan_id\": \"41\",
+    \"abonement\": false,
+    \"services_cost\": \"\",
+    \"name\": \"vm17803\",
+    \"state\": \"running\",
+    \"changed\": \"2018-09-18 19:40:05\",
+    \"status\": \"enabled\",
+    \"vds_template_id\": \"22\",
+    \"mac_address\": \"00:25:90:00:45:8B\",
+    \"secret\": \"\",
+    \"abonement_cost\": 23880,
+    \"can_delete\": true
+  }
+}")
+
+(test-begin "vm")
+
+(test-assert "gms-vm"
+  (mock ((gms scripts vm) fetch-vm
+         (match-lambda
+           ("vm12345" test-vm-json)
+           (_ (error "Nonexistent account: " account))))
+        (and (string=? (with-output-to-string
+                         (lambda ()
+                           (gms-vm "show" "vm12345")))
+                       "\
+created: 2016-08-17 05:47:24
+changed: 2018-09-18 19:40:05
+status: enabled
+state: running
+template_id: 22
+client_id: 4653
+vnc_port: 5955
+
+")
+             (string=? (with-output-to-string
+                         (lambda ()
+                           (gms-vm "ip" "vm12345")))
+                       "\
+ip_address: 78.108.95.38
+isp_license: #t
+
+")
+             (string=? (with-output-to-string
+                         (lambda ()
+                           (gms-vm "plan" "vm12345")))
+                       "\
+created: 2014-07-01 16:54:18
+name: A-2
+admin: #t
+
+")
+             (string=? (with-output-to-string
+                         (lambda ()
+                           (gms-vm "server" "vm12345")))
+                       "\
+name: kvm27
+
+")
+             (string=? (pk (with-output-to-string
+                         (lambda ()
+                           (gms-vm "template" "vm12345"))))
+                       "\
+name: Debian Linux 8 KVM (ISP Manager)
+URI: http://mirror.yandex.ru/debian/dists/jessie/main/installer-amd64/
+
+"))))
+
+(test-end "vm")
+
+
