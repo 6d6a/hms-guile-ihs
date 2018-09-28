0b850a62487365f54e561ea2b6d74ad93083aa5f
Oleg Pykhalov <go.wigust@gmail.com>
2018-09-28 21:16:43 +0300

    Indent copyright.
    * build-aux/test-driver.scm: Indent copyright.
    * gms/config.scm.in: Indent copyright.
    * gms/scripts.scm: Indent copyright.
    * gms/scripts/account.scm: Indent copyright.
    * gms/scripts/account/history.scm: Indent copyright.
    * gms/scripts/account/search.scm: Indent copyright.
    * gms/scripts/pull.scm: Indent copyright.
    * gms/scripts/server.scm: Indent copyright.
    * gms/ui.scm: Indent copyright.
    * gms/utils.scm: Indent copyright.
    * scripts/gms.in: Indent copyright.

--- build-aux/test-driver.scm
+++ build-aux/test-driver.scm
@@ -1,21 +1,21 @@
 ;;;; test-driver.scm - Guile test driver for Automake testsuite harness
 
-(define script-version "2018-09-25.01") ;UTC
+(define script-version "2018-09-28.18") ;UTC
 
 ;;; Copyright © 2015, 2016 Mathieu Lirzin <mthl@gnu.org>
 ;;;
-;;; This program is free software; you can redistribute it and/or modify it
-;;; under the terms of the GNU General Public License as published by
-;;; the Free Software Foundation; either version 3 of the License, or (at
-;;; your option) any later version.
+;;; This program is free software; you can redistribute it and/or
+;;; modify it under the terms of the GNU General Public License as
+;;; published by the Free Software Foundation; either version 3 of the
+;;; License, or (at your option) any later version.
 ;;;
-;;; This program is distributed in the hope that it will be useful, but
-;;; WITHOUT ANY WARRANTY; without even the implied warranty of
-;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-;;; GNU General Public License for more details.
+;;; This program is distributed in the hope that it will be useful,
+;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;;; General Public License for more details.
 ;;;
 ;;; You should have received a copy of the GNU General Public License
-;;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+;;; along with Guile GMS.  If not, see <http://www.gnu.org/licenses/>.
 
 ;;;; Commentary:
 ;;;
--- gms/scripts/account.scm
+++ gms/scripts/account.scm
@@ -3,18 +3,18 @@
 ;;;
 ;;; This file is part of Guile GMS.
 ;;;
-;;; Guile GMS is free software; you can redistribute it and/or modify it under
-;;; the terms of the GNU General Public License as published by the Free
-;;; Software Foundation; either version 3 of the License, or (at your option)
-;;; any later version.
+;;; Guile GMS is free software; you can redistribute it and/or modify
+;;; it under the terms of the GNU General Public License as published
+;;; by the Free Software Foundation; either version 3 of the License,
+;;; or (at your option) any later version.
 ;;;
-;;; Guile GMS is distributed in the hope that it will be useful, but WITHOUT
-;;; ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
-;;; FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
-;;; more details.
+;;; Guile GMS is distributed in the hope that it will be useful, but
+;;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;;; General Public License for more details.
 ;;;
-;;; You should have received a copy of the GNU General Public License along
-;;; with Guile GMS.  If not, see <http://www.gnu.org/licenses/>.
+;;; You should have received a copy of the GNU General Public License
+;;; along with Guile GMS.  If not, see <http://www.gnu.org/licenses/>.
 
 (define-module (gms scripts account)
   #:use-module ((guix scripts) #:select (parse-command-line))
